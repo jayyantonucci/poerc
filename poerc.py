288735b6fcc0f8a9894840e01096ca6def0ded39
jayyantonucci <75542433+jayyantonucci@users.noreply.github.com>
2021-03-19 23:54:28 -0400

    first commit

---
+++ poerc.py
@@ -0,0 +1,24 @@
+#gear piece [Fire, Cold, Lightning, Chaos]
+
+gloves = [20, 15, 0, 10]
+boots = [0, 15, 0, 5]
+helm = [5, 15, 0, 15]
+
+fire = 0
+cold = 0
+lightning = 0
+chaos = 0
+
+items = [gloves, boots, helm]
+
+
+for i in items:
+    fire = fire + i[0]
+    cold = cold + i[1]
+    lightning = lightning + i[2]
+    chaos = chaos + i[3]
+
+res = [fire, cold, lightning, chaos]
+
+
+print(res)
