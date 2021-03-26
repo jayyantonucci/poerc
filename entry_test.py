d322ec54e1b018efdc2c7ac7a0a143b2ca307024
jayyantonucci <jayyantonucci@gmail.com>
2021-03-25 21:21:02 -0400

    entry-module-commit

--- entry_test.py
+++ entry_test.py
@@ -1,65 +1,35 @@
 import tkinter as tk
-fullstring = """Diamond Ring
-+50 to Strength 
-+28% to Fire Resistance
-+8% to Cold Resistance
-some extra text"""
 
 window = tk.Tk()
 window.geometry("600x400")
-
 item_var = tk.StringVar()
 
 
-def check_item():
-    print(userstring)
-    splitstring = fullstring.splitlines()
-    lines = len(splitstring)
-    for i in range(lines):
-        if splitstring[i].find("% to Fire Resistance") != -1:
-            x = splitstring[i].split("%")
-            r1 = x[0].replace("+", "")
-            print(r1, "fire")
-        if splitstring[i].find("% to Cold Resistance") != -1:
-            x = splitstring[i].split("%")
-            r2 = x[0].replace("+", "")
-            print(r2, "cold")
-        if splitstring[i].find("% to Lightning Resistance") != -1:
-            x = splitstring[i].split("%")
-            r3 = x[0].replace("+", "")
-            print(r3, "lightning")
-        if splitstring[i].find("% to Chaos Resistance") != -1:
-            x = splitstring[i].split("%")
-            r4 = x[0].replace("+", "")
-            print(r4, "chaos")
-
-
 def submit():
-    item = item_var.get()
-    print(item)
-    splitstring = item.splitlines()
+    r1 = r2 = r3 = r4 = 0
+    a = item_var.get()
+    splitstring = a.splitlines()
     lines = len(splitstring)
+    n = splitstring[0]
     for i in range(lines):
         if splitstring[i].find("% to Fire Resistance") != -1:
             x = splitstring[i].split("%")
             r1 = x[0].replace("+", "")
-            print(r1, "fire")
         if splitstring[i].find("% to Cold Resistance") != -1:
             x = splitstring[i].split("%")
             r2 = x[0].replace("+", "")
-            print(r2, "cold")
         if splitstring[i].find("% to Lightning Resistance") != -1:
             x = splitstring[i].split("%")
             r3 = x[0].replace("+", "")
-            print(r3, "lightning")
         if splitstring[i].find("% to Chaos Resistance") != -1:
             x = splitstring[i].split("%")
             r4 = x[0].replace("+", "")
-            print(r4, "chaos")
+    b = [n, r1, r2, r3, r4, a]
+    entry.delete(0, 'end')
 
 
-userstring = tk.Entry(window, textvariable=item_var)
-userstring.pack()
+entry = tk.Entry(window, textvariable=item_var)
+entry.pack()
 button = tk.Button(window, text="enter", command=submit)
 button.pack()
 
